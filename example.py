1832f9292e2f66b388f9cb7340cd73498941743a
Melissa Munoz <74799632+MelissaSMunoz@users.noreply.github.com>
2020-12-16 05:25:38 -0500

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import ADM
 
+
 #Physical properties of the star
 #-------------------------------------------------------------------------------
 #Effective temperature, Teff, is in Kelvins
@@ -9,7 +10,7 @@ import ADM
 #Stellar radius, Rstar, is in solar radius
 #Terminal velocity, Vinf, is in km/s
 #Mass-loss rate, Mdot, is in solar mass per year
-#Polar field strength, Bstar is in Gauss
+#Polar field strength, Bstar, is in Gauss
 Teff = 35000.0 
 Mstar = 30.0
 Rstar = 10.
@@ -17,16 +18,18 @@ Vinf = 2500.0
 Mdot = 10**(-6.0)
 Bstar = 2500.0
 
+
 #Geometric angles
 #-------------------------------------------------------------------------------
-#inclination angle, inc, in degrees
-#magnetic obliquity, beta, in degrees
+#Inclination angle, inc, in degrees
+#Magnetic obliquity, beta, in degrees
 inc = 30.
 beta = 60.
 A = inc+beta
 B = np.abs(inc-beta)
 
-#Extra paramaters
+
+#Extra parameters
 #-------------------------------------------------------------------------------
 #Smoothing length, delta
 #Interstellar polarisation, QIS and UIS
@@ -35,6 +38,7 @@ delta = 0.1
 QIS = 0.0
 UIS = 0.0
 thetaIS = 0.0
+
 
 #Calling ADM
 #-------------------------------------------------------------------------------
@@ -45,17 +49,25 @@ outp = ADM.POLp(phi, inc, beta, Nx, Ny, Nz, Teff, Mstar, Rstar, Vinf, Mdot, Bsta
 #Finite star (slower, but perhaps more accurate)
 out = ADM.POL(phi, inc, beta, Nx, Ny, Nz, Teff, Mstar, Rstar, Vinf, Mdot, Bstar, delta, QIS, UIS, thetaIS)
 
+
 #Plotting phased Stokes Q and U curves
 #-------------------------------------------------------------------------------
-#plt.figure()
-#plt.plot(phi,outp[0])
-#plt.plot(phi,outp[1])
-#plt.plot(phi,out[0])
-#plt.plot(phi,out[1])
-#plt.show()
-plt.figure()
-plt.plot(outp[0],outp[1])
-plt.plot(out[0],out[1])
+fig, ax = plt.subplots(figsize=(9,6),sharey=True)
+ax[0].plot(phi, outp[0],'k')
+ax[0].plot(phi+1, outp[0],'k')
+ax[0].plot(phi-1, outp[0],'k')
+ax[1].plot(phi, outp[1],'k')
+ax[1].plot(phi+1, outp[1],'k')
+ax[1].plot(phi-1, outp[1],'k')
+ax[0].set_ylabel('Q [%]',fontsize=14)
+ax[1].set_ylabel('U [%]',fontsize=14)
+ax[1].set_xlabel('Rotational phase',fontsize=14)
 plt.show()
 
+plt.figure(figsize=(6,6))
+plt.plot(outp[0],outp[1])
+plt.plot(out[0],out[1])
+plt.ylabel('Q [%]',fontsize=14)
+plt.xlabel('U [%]',fontsize=14)
+plt.show()
 
